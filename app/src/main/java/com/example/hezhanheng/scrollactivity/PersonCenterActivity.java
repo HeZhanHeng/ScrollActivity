a40570a68f7696bd5288237359f1637c35a4ffec
heng <1104791552@qq.com>
2020-05-12 10:32:53 +0800

    标题渐变优化

--- app/src/main/java/com/example/hezhanheng/scrollactivity/PersonCenterActivity.java
+++ app/src/main/java/com/example/hezhanheng/scrollactivity/PersonCenterActivity.java
@@ -1,5 +1,6 @@
 package com.example.hezhanheng.scrollactivity;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -28,25 +29,30 @@ public class PersonCenterActivity extends AppCompatActivity implements MyScrollV
         title=(TextView)findViewById(R.id.title);
         shopping_cart = (ImageView) findViewById(R.id.shopping_cart);
         scroller = (MyScrollView) findViewById(R.id.scroller);
-        top_bg.getBackground().setAlpha(0);
+        top_bg.getBackground().setAlpha(255);
         scroller.setScrolListener(this);
     } 	@Override
     public void onScroll(int scrollY) {
         if(scrollY < 100){
-            top_bg.getBackground().setAlpha(0);
-            back.getBackground().setAlpha(255);
-            shopping_cart.getBackground().setAlpha(255);
+            top_bg.getBackground().setAlpha(255);
+            top_bg.setBackgroundColor(getResources().getColor(R.color.lemonchiffon));
+//            back.getBackground().setAlpha(255);
+//            shopping_cart.getBackground().setAlpha(255);
             title.setVisibility(View.INVISIBLE);
-        }else if(scrollY >= 100 && scrollY < 860){
-            top_bg.getBackground().setAlpha((scrollY-100)/3);
+        }else if(scrollY >= 100 && scrollY < 500){
+            top_bg.setBackgroundColor(getResources().getColor(R.color.lemonchiffon));
+            top_bg.getBackground().setAlpha(255-(scrollY-100)/3);
+            title.setVisibility(View.INVISIBLE);
 //            back.getBackground().setAlpha(255 - (scrollY-100)/3);
-            shopping_cart.getBackground().setAlpha(255 - (scrollY-100)/3);
+//            shopping_cart.getBackground().setAlpha(255 - (scrollY-100)/3);
 
         }else{
-            top_bg.getBackground().setAlpha(255);
+            top_bg.getBackground().setAlpha(0);
+            top_bg.setBackgroundColor(Color.rgb(255, 255, 255));
             title.setVisibility(View.VISIBLE);
+//            top_bg.setBackgroundColor(getResources().getColor(R.color.white));
 //        back.getBackground().setAlpha(0);
-        shopping_cart.getBackground().setAlpha(0);
+//        shopping_cart.getBackground().setAlpha(0);
         }
     }
 
